d07c7af4e3ef65bab8884c585bd236ffb5f26f33
ericnkaka <nkakaeric96@gmail.com>
2023-12-18 17:37:32 +0200

    feat: done

--- e-pro/src/app/app.component.ts
+++ e-pro/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCaretDown,
+  faSearch,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
-import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { CheckoutComponent } from '../pages/checkout/checkout.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +18,26 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
   imports: [CommonModule, RouterOutlet, CheckoutComponent, FontAwesomeModule],
 })
 export class AppComponent {
-  faCoffee = faCaretDown;
-  fab = faCartShopping;
+  isOpen = false;
+
+  handleClick(): void {
+    console.log('button is clicked');
+    this.isOpen = !this.isOpen;
+  }
+  caretDown = faCaretDown;
+  cart = faCartShopping;
+  search = faSearch;
+  user = faUser;
+
+  menuItems = [{ label: 'Home' }, { label: 'Pages' }, { label: 'contact us' }];
+  products = [
+    'Fashion',
+    'Fishion 1',
+    'Fishion 2',
+    'Fishion 3',
+    'Fishion 4',
+    'Electornics 2',
+    'Electornics 3',
+  ];
+  categories = ['New Arrival', 'Best Sellers', 'Featured', 'Special Offer'];
 }
